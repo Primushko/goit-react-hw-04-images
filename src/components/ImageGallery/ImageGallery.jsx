d58a5431fbf1e8626bc3a75e7cfd0b12a919a37a
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-06-08 22:05:15 +0300

    imageGallleryItem useState

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 import { Gallery } from './ImageGallery.module';
 // компонент `ImageGallery`приймає масив об'єктів `galleryItems` та відображає галерею зображень,
@@ -16,3 +17,6 @@ export const ImageGallery = ({ galleryItems }) => {
 // метод `map`створює компонент `ImageGalleryItem` для кожного об'єкта `galleryItem` в масиві.
 // Кожен елемент отримує унікальний ключ `key` на основі `id` об'єкта `galleryItem`,
 //  і передається сам об'єкт `galleryItem` через проп `galleryItem`.
+ImageGallery.propTypes = {
+  galleryItems: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,36 +1,67 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Modal } from 'components/Modal/Modal';
 import { GalleryItem, GalleryImg } from './ImageGalleryItem.module';
-import { Component } from 'react';
-// компонент `ImageGalleryItem` відображає елемент галереї з зображенням.
-//Коли користувач клікає на зображення, відкривається модальне вікно, яке показує велике зображення.
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false, // початковий стан `isModalOpen` визначає, чи відкрите модальне вікно
+// import { Component } from 'react';
+export const ImageGalleryItem = ({
+  galleryItem: { webformatURL, largeImageURL, tags },
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(isModalOpen => !isModalOpen);
   };
 
-  toggleModal = () => {
-    // Метод `toggleModal` встановлює стан `isModalOpen` протилежним до поточного значення.
-    this.setState(({ isModalOpen }) => ({ isModalOpen: !isModalOpen }));
-  }; // дозволяє відкривати або закривати модальне вікно після кліку на елемент галереї.
+  return (
+    <>
+      <GalleryItem className="gallery-item" onClick={toggleModal}>
+        <GalleryImg src={webformatURL} alt={tags} />
+      </GalleryItem>
+      {isModalOpen && (
+        <Modal
+          largeImageURL={largeImageURL}
+          alt={tags}
+          onCloseModal={toggleModal}
+        />
+      )}
+    </>
+  );
+};
+ImageGalleryItem.propTypes = {
+  galleryItem: PropTypes.shape({
+    webformatURL: PropTypes.string.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
-  render() {
-    const {
-      galleryItem: { webformatURL, largeImageURL, tags },
-    } = this.props;
+// export class ImageGalleryItem extends Component {
+//   state = {
+//     isModalOpen: false,
+//   };
 
-    return (
-      <>
-        <GalleryItem className="gallery-item" onClick={this.toggleModal}>
-          <GalleryImg src={webformatURL} alt={tags} />
-        </GalleryItem>
-        {this.state.isModalOpen && (
-          <Modal
-            largeImageURL={largeImageURL}
-            alt={tags}
-            onCloseModal={this.toggleModal}
-          />
-        )}
-      </>
-    );
-  }
-}
+//   toggleModal = () => {
+//     this.setState(({ isModalOpen }) => ({ isModalOpen: !isModalOpen }));
+//   };
+
+//   render() {
+//     const {
+//       galleryItem: { webformatURL, largeImageURL, tags },
+//     } = this.props;
+
+//     return (
+//       <>
+//         <GalleryItem className="gallery-item" onClick={this.toggleModal}>
+//           <GalleryImg src={webformatURL} alt={tags} />
+//         </GalleryItem>
+//         {this.state.isModalOpen && (
+//           <Modal
+//             largeImageURL={largeImageURL}
+//             alt={tags}
+//             onCloseModal={this.toggleModal}
+//           />
+//         )}
+//       </>
+//     );
+//   }
+// }
